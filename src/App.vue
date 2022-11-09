1e95385c3b0f214ec023eac596423fddcbe618ab
pietrobrighella <info@pietrobrighella.com>
2022-11-09 13:19:12 +0100

    jumbo

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
     <AppTitle/>
+    <AppHero/>
   </div>
 </template>
 
 <script>
   import AppTitle from './components/AppTitle.vue';
+  import AppHero from './components/AppHero.vue';
   export default {
     components: {
       AppTitle,
+      AppHero,
     },
     data(){
 
---
+++ src/components/AppHero.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="jumbo">
+        <h1>{{title}}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'AppHero',
+        data() {
+            return {
+                title: 'Titolo Jumbo',
+            }
+        }
+    }
+</script>
+
+<style lang="css" scoped>
+.jumbo {
+    width: 100%;
+    height: 500px;
+    background-color: lightgray;
+}
+
+h1 {
+    font-size: 96px;
+    font-weight: 900;
+    text-align: center;
+}
+</style>
