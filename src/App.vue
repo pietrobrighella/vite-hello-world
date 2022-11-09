43de697637a9907b9edbc722645ee17c3633e63c
pietrobrighella <info@pietrobrighella.com>
2022-11-09 13:12:49 +0100

    title

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <AppTitle/>
+  </div>
+</template>
+
+<script>
+  import AppTitle from './components/AppTitle.vue';
+  export default {
+    components: {
+      AppTitle,
+    },
+    data(){
+
+    }
+  }
+</script>
+
+<style lang="css" scoped>
+
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,24 @@
+<template>
+    <header>
+        <h1>{{titolo}}</h1>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: 'AppTitle',
+        data() {
+            return {
+                titolo: 'La mia prima app con Vite!'
+            }
+        }
+    }
+</script>
+
+<style lang="css" scoped>
+header {
+    width: 100%;
+    height: 70px;
+    padding: 20px;
+}
+</style>
